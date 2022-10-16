c2268a96409c7190138026c029b6878f006a1dc2
Yevgeniy Gavrilov <y@gavrilov.kz>
2022-10-16 09:02:48 +0600

    cleaning up

--- webroot/index.php
+++ webroot/index.php
@@ -2,12 +2,9 @@
 
     
 
-    require $_SERVER['APP_HOME'] . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoload.php';
+    require '..' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 	use src\App;
 
 	$app = new App();
 	$app->run();
-
-    // var_dump(getallheaders());
-    // var_dump($_SERVER);
