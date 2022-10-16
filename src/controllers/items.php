d6a59a857319b0bb0842f43caf21ac81c077f8bb
Yevgeniy Gavrilov <y@gavrilov.kz>
2022-10-16 18:07:02 +0600

    added create function

--- src/controllers/items.php
+++ src/controllers/items.php
@@ -6,11 +6,20 @@ class items {
 
     private $items = [];
 
+    private $items_file_name = '../storage/items.json';
+
+    private $model = [
+        'name' => FILTER_SANITIZE_STRING,
+        'comp' => FILTER_SANITIZE_STRING,
+        'desc' => FILTER_SANITIZE_STRING,
+        'price' => FILTER_SANITIZE_NUMBER_INT
+    ];
+
     public function __construct()
     {
         //  initialize items
         header('Content-Type:application/json');
-        $this->items = json_decode(file_get_contents('../storage/items.json'));
+        $this->items = (array)json_decode(file_get_contents($this->items_file_name));
     }
 
     public function list()
@@ -92,6 +101,41 @@ class items {
             );
             return;
         }
+
+        //  sanitize data
+        //  validate data
+        //  create a new record
+        //  return new record
+        
+        $new_record = [];
+        foreach ($this->model as $field_name => $field_filter)
+        {
+            if (array_key_exists($field_name, $_POST) === false) {
+                continue;
+            }
+            $new_record[$field_name] = filter_var($_POST[$field_name], $field_filter);
+        }
+
+        $max_id = 1;
+        foreach ($this->items as $item) 
+        {
+            if ($item->id > $max_id) 
+            {
+                $max_id = $item->id;
+            }
+        }
+        $new_record['id'] = $max_id + 1;
+        $this->items[$new_record['id']] = $new_record;
+        file_put_contents($this->items_file_name, json_encode($this->items));
+
+        echo json_encode(
+            [
+                'code'          => 201,
+                'message'       => 'new record created',
+                'new_record'    => $new_record
+            ]
+        );
+        return;
     }
 
 }
